9402900e4f022a70251d502e99839afe34a5e276
teiserse <teiserse@tcd.ie>
2019-03-05 20:25:11 +0000

    created testing script

---
+++ runtest.sh
@@ -0,0 +1,29 @@
+config="1000 1000 5 10 10"
+length=2
+
+if [[ $# = 1 ]] ; then
+	RESULT_FILE=$1
+else
+	RESULT_FILE=results.txt
+fi
+
+if [[ -e $RESULT_FILE ]] ; then
+	echo "File '$RESULT_FILE' already exists, not running tests."
+	exit 1
+fi
+
+echo "Running on: $(hostname). Values are in microseconds." >> $RESULT_FILE
+echo "Test running using parameters \"$config\" for $length iterations:" >> $RESULT_FILE
+
+run=1
+while [[ $run -le $length ]]
+do
+	echo "Running iteration $run out of $length."
+	res=`./a.out $config | grep Team`
+	if ! [[ $res =~ ^.*\ ([0-9]+)\ .*$ ]] ; then
+		echo $res
+	else
+		echo ${BASH_REMATCH[1]} >> $RESULT_FILE
+	fi
+	((run = run + 1))
+done
